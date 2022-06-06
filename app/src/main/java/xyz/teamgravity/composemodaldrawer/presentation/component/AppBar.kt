68f1ee719b0406f962b6af62a0ddf175e76612fe
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-06 16:07:46 +0500

    add app bar

---
+++ app/src/main/java/xyz/teamgravity/composemodaldrawer/presentation/component/AppBar.kt
@@ -0,0 +1,32 @@
+package xyz.teamgravity.composemodaldrawer.presentation.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import xyz.teamgravity.composemodaldrawer.R
+
+@Composable
+fun AppBar(
+    drawerOpen: Boolean,
+    onNavigationClick: () -> Unit,
+) {
+    SmallTopAppBar(
+        title = {
+            Text(text = stringResource(id = R.string.app_name))
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationClick) {
+                Icon(
+                    imageVector = if (drawerOpen) Icons.Default.Close else Icons.Default.Menu,
+                    contentDescription = stringResource(id = R.string.cd_toggle_drawer)
+                )
+            }
+        }
+    )
+}
